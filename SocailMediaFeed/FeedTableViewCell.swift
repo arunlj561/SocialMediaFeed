540673163d2c9e0f0e5b3ff3e27119a566c20691
Arun <arunlj561@gmail.com>
2020-07-02 17:43:46 +0530

    Created FeedTableView Cell

---
+++ SocailMediaFeed/FeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedTableViewCell.swift
+//  SocailMediaFeed
+//
+//  Created by Arun Jangid on 02/07/20.
+//  Copyright © 2020 Arun Jangid. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
