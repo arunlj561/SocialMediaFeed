41b7dd10b9682118859da75899e1b1c21f606139
Arun <arunlj561@gmail.com>
2020-07-02 17:37:35 +0530

    Initial Commit

---
+++ SocailMediaFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocailMediaFeed
+//
+//  Created by Arun Jangid on 02/07/20.
+//  Copyright © 2020 Arun Jangid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
